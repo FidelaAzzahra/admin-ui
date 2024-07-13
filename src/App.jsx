13adc67189a20e87415f1c7a65b4c7bd654343e8
Fidela Azzahra <fidela.meruem@gmail.com>
2024-07-13 17:14:51 +0700

    testing kemarin

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./style/dark.scss";
-// import pages yang digunakan
+//import pages yang digunakan
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import List from "./pages/list/List";
